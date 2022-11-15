a93ef59569075e4d2d93181164d4517e81a04fd2
buici <115681379+buici@users.noreply.github.com>
2022-11-14 22:04:28 -0500

    Add files via upload

---
+++ Assignment11/Main/Main.py
@@ -0,0 +1,28 @@
+'''
+Name: Colin Bui, Mahaly Marcelin
+email: buici@mail.uc.edu, marcelml@mail.uc.edu
+Assignment: Assignment 11
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: 
+Anything else that's relevant: 
+'''
+
+import json, requests
+
+#API: 6GdzhIKPz8qS2dkdPkZMxwhgl6fziXxQ7g6jfbEu
+response = requests.get('https://api.nasa.gov/planetary/apod?api_key=6GdzhIKPz8qS2dkdPkZMxwhgl6fziXxQ7g6jfbEu')
+
+json_string = response.content
+
+parsed_json = json.loads(json_string) 
+
+#prints python dictionary    
+print(parsed_json)
+
+date = (parsed_json['date'])
+explanation = (parsed_json['explanation'])
+
+#shows date & explanation of Picture of the day
+print(date)
+print(explanation)
